90179814563f0a18ce0459b3cc097abebdab1a24
yongveloper <aaa9015@naver.com>
2024-03-25 16:33:02 +0900

    feat: jotai 예제 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import { useAtom } from 'jotai';
+import { countAtom } from './features/counter/atom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useAtom(countAtom);
+
+  const onIncrementButtonClick = () => setCount((count) => count + 1);
+
+  const onDecrementButtonClick = () => setCount((count) => count - 1);
 
   return (
     <>
       <h1>리액트 상태 관리 라이브러리 에제</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment value
-        </button>
+        <button onClick={onIncrementButtonClick}>Increment value</button>
       </div>
       <span>{count}</span>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Decrement value
-        </button>
+        <button onClick={onDecrementButtonClick}>Decrement value</button>
       </div>
     </>
   );
