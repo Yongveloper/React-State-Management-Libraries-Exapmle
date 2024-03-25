9e395a24252c1b920c60543df7501d42b74df86f
yongveloper <aaa9015@naver.com>
2024-03-25 15:09:39 +0900

    refactor: 예제 기본 화면 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -8,18 +6,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>리액트 상태 관리 라이브러리 에제</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment value
+        </button>
+      </div>
+      <span>{count}</span>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Decrement value
         </button>
       </div>
     </>
