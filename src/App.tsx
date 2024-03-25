384dce59eead558ac9699b8eed605730922d5eb1
yongveloper <aaa9015@naver.com>
2024-03-25 15:07:52 +0900

    feat: rtk 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './store';
+import { decrement, increment } from './features/counter/counterSlice';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const onIncrementButtonClick = () => dispatch(increment());
+
+  const onDecrementButtonClick = () => dispatch(decrement());
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>리액트 상태 관리 라이브러리 에제</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={onIncrementButtonClick}>Increment value</button>
+      </div>
+      <span>{count}</span>
+      <div className="card">
+        <button onClick={onDecrementButtonClick}>Decrement value</button>
       </div>
     </>
   );
