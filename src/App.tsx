1dcd91612dfeffcd4a531cfcbae61713887fbeb7
yongveloper <aaa9015@naver.com>
2024-03-25 15:15:42 +0900

    feat: recoil 예제 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
 import './App.css';
+import { useRecoilState } from 'recoil';
+import { countState } from './features/counter/atom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useRecoilState(countState);
+
+  const onIncrementButtonClick = () => setCount((count) => count + 1);
+
+  const onDecrementButtonClick = () => setCount((count) => count - 1);
 
   return (
     <>
       <h1>리액트 상태 관리 라이브러리 에제</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment value
-        </button>
+        <button onClick={onIncrementButtonClick}>Increment value</button>
       </div>
       <span>{count}</span>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Decrement value
-        </button>
+        <button onClick={onDecrementButtonClick}>Decrement value</button>
       </div>
     </>
   );
